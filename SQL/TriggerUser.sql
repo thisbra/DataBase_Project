9d5c8bc0df1bd69ba7fec58a7efd8970622e505a
josecarlos55 <46457623+josecarlos55@users.noreply.github.com>
2022-06-20 21:25:36 +0100

    Other pushes, Thiago Brasil

---
+++ SQL/TriggerUser.sql
@@ -0,0 +1,24 @@
+DROP TRIGGER createUser
+GO
+
+CREATE TRIGGER createUser ON usuario
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DECLARE @NIF AS VARCHAR(9);
+    --DECLARE @Nome AS VARCHAR(40); 
+    SELECT @NIF = NIF FROM inserted;
+    IF EXISTS(SELECT 1 FROM CONTA WHERE NIF_Usuario IS NULL)
+    BEGIN
+    BEGIN TRY
+		UPDATE CONTA
+		SET  NIF_Usuario=@NIF, DATA = GETDATE(), ID_CONTA = (SELECT TOP 1 * FROM CONTA ORDER BY ID_Conta DESC) + 1  
+    END TRY
+    BEGIN CATCH
+        raiserror ('Não foi possível inserir o usuario', 16, 1);
+        ROLLBACK TRAN
+    END CATCH
+	END
+END
+GO
